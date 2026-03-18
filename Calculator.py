bf09759d18908562434c2af7af9f739115b8ae0b
Yaeshveer Naidoo <yaeshveer@gmail.com>
2026-03-18 15:36:56 +0200

    Initial commit: basic calculator

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+print("When your two numbers are added they equal: " + str(add(x, y)))
+print("When your two numbers are subtracted they equal: " + str(subtract(x, y)))
