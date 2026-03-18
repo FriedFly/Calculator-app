a682547735a3f8a07e1f205671a9517ca07309d8
Yaeshveer Naidoo <yaeshveer@gmail.com>
2026-03-18 15:46:12 +0200

    Added divide function

--- Calculator.py
+++ Calculator.py
@@ -4,9 +4,13 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+def multiple(x, y):
+    return x * y
+
 
 x = int(input("Enter the first number: "))
 y = int(input("Enter the second number: "))
 
 print("When your two numbers are added they equal: " + str(add(x, y)))
 print("When your two numbers are subtracted they equal: " + str(subtract(x, y)))
+print("When your two numbers are subtracted they equal: " + str(multiple(x, y)))
