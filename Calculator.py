fd77b03634f0a6cef8826288b88b5c644474a919
Yaeshveer Naidoo <yaeshveer@gmail.com>
2026-03-18 15:48:50 +0200

    Added Multiply function

--- Calculator.py
+++ Calculator.py
@@ -7,10 +7,17 @@ def subtract(x, y):
 def multiple(x, y):
     return x * y
 
+def divide(x, y):
+    if x == 0:
+        return "Cannot divide by zero"
+    else:
+        return x * y
+
 
 x = int(input("Enter the first number: "))
 y = int(input("Enter the second number: "))
 
 print("When your two numbers are added they equal: " + str(add(x, y)))
 print("When your two numbers are subtracted they equal: " + str(subtract(x, y)))
-print("When your two numbers are subtracted they equal: " + str(multiple(x, y)))
+print("When your two numbers are multiplied they equal: " + str(multiple(x, y)))
+print("When your two numbers are divided they equal: " + str(divide(x, y)))
